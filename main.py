9f8c078570d521a6d7a80b552183a2bd54025b62
Nay Myo Htet <naymyo.htet@wavemoney.com.mm>
2024-08-28 22:05:49 +0630

    query parameter

--- main.py
+++ main.py
@@ -15,10 +15,23 @@ BANDS = [
 
 
 @app.get('/bands')
-async def bands() -> list[Band]:
-    return [
-        Band(**b) for b in BANDS
-    ]
+async def bands(
+    genre: GenreURLChoices | None = None,
+    has_albums: bool = False
+) -> list[Band]:
+    band_list = [Band(**b) for b in BANDS]
+
+    if genre:
+        band_list = [
+            b for b in band_list if b.genre.lower() == genre.value
+        ]
+
+    if has_albums:
+        band_list = [
+            b for b in band_list if len(b.albums) > 0
+        ]
+
+    return band_list
 
 
 @app.get('/bands/{band_id}')
