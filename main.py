cf729821510712182fbb6a4b631c0b55a970b4bc
Nay Myo Htet <naymyo.htet@wavemoney.com.mm>
2024-08-28 20:59:55 +0630

    Http exception and enum

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from enum import Enum
 
 app = FastAPI()
 
 
-@app.get('/')
-async def index() -> dict[str, str]:
-    return {'hello': 'world'}
+class GenreURLChoices(Enum):
+    ROCK = 'rock'
+    ELECTRONIC = 'electronic'
+    METAL = 'metal'
+    HIP_HOP = 'hip-hop'
 
 
-@app.get('/about')
-async def about() -> str:
-    return 'An Exceptional Company'
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+    {'id': 3, 'name': 'Slowdive', 'genre': 'Shoegaze'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
+]
+
+
+@app.get('/bands')
+async def bands() -> list[dict]:
+    return BANDS
+
+
+@app.get('/bands/{band_id}')
+async def band(band_id: int) -> dict:
+    band = next((b for b in BANDS if b['id'] == band_id), None)
+    if band is None:
+        # status code 404
+        raise HTTPException(status_code=404, detail='Band not found')
+    return band
+
+
+@app.get('/bands/genre/{genre}')
+async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
+    return [
+        b for b in BANDS if b['genre'].lower() == genre.value
+    ]
